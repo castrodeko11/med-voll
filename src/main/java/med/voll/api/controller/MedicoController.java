2d1ef0f7823912840fed22b1b2c416b1a42171be
castrodeko11 <castrodeko11@hotmail.com>
2023-03-06 08:49:34 -0300

    Update Doctor and use Transactional

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -5,15 +5,14 @@ import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
-import med.voll.api.medicos.DadosListagemMedico;
+import med.voll.api.medico.DadosListagemMedico;
+import med.voll.api.medicos.DadosAtualizacaoMedico;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -32,6 +31,13 @@ public class MedicoController {
     @GetMapping
     public Page<DadosListagemMedico> listar(@PageableDefault(size = 15, page = 0, sort = {"nome"} ) Pageable pageable) {
         return medicoRepository.findAll(pageable).map(DadosListagemMedico::new);
+    }
+
+    @PutMapping
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados){
+        var medico = medicoRepository.getReferenceById(dados.id());
+        medico.atualizarinformacoes(dados);
     }
 
 }
