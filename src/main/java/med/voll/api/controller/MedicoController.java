345365bcefa70d580b98023eb28c0c6391c86596
castrodeko11 <castrodeko11@hotmail.com>
2023-03-06 08:21:36 -0300

    Create List of Doctor and Pageble for DTO

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -5,9 +5,15 @@ import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
+import med.voll.api.medicos.DadosListagemMedico;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -21,6 +27,11 @@ public class MedicoController {
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
 
         medicoRepository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 15, page = 0, sort = {"nome"} ) Pageable pageable) {
+        return medicoRepository.findAll(pageable).map(DadosListagemMedico::new);
     }
 
 }
